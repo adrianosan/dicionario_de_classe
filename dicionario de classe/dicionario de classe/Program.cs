479d6cf0e0cc52eaf3c3e675662b81aa3bda56c6
adriano.santos <adriano.santos@squadra.com.br>
2019-06-17 10:37:10 -0300

    dicionario de classe

---
+++ dicionario de classe/dicionario de classe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dicionario_de_classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            IDictionary<string, string> hashclinte = new Dictionary<string, string>();
+
+            IDictionary<string, string> hashpedido = new Dictionary<string, string>();
+
+            IDictionary<string, string> hashproduto = new Dictionary<string, string>();
+
+            IDictionary<string, string> hashfornecedores = new Dictionary<string, string>();
+
+
+            
+        }
+    }
+}
